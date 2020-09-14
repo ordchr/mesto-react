4a1455bc27abb59766dec3c05081a0e93273fc56
Антон <mongoostic@yandex.ru>
2020-09-14 23:47:37 +0800

    Проектная работа №10. Одним коммитом, т.к. делал по описанию работы, и как-то незаметно собрал проект

---
+++ src/components/Card.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import delIcon from '../images/del-icon.svg';
+
+function Card({card, onCardClick}) {
+
+  function handleCardClick() {
+    onCardClick(card);
+  }
+  const {name, link, likes} = card;
+  return (
+    <div className="place">
+      <img src={link} className="place__image" alt="" onClick={handleCardClick} />
+      <button type="button" className="place__image-del" style={{ backgroundImage: `url(${delIcon})` }}></button>
+      <div className="place__title">
+        <h3 className="place__title-text">{card.name}</h3>
+        <div className="place__title-like-block">
+          <button type="button" className="place__title-like"></button>
+          <h5 className="place__title-like-count">{likes.length}</h5>
+        </div>
+      </div>
+    </div>
+
+  );
+
+}
+
+export default Card;
---
+++ src/components/ImagePopup.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function ImagePopup({
+  card,
+  onClose
+}) {
+  console.log(card);
+  return (
+      <div className={`popup-preview ${card ? 'popup_opened' : 'popup_closed' }`}>
+        <div className="popup-container popup-preview__container">
+          <img src={card.link} className="popup-preview__image" alt="" />
+          <button type="button" className="popup__button-close" onClick={onClose}></button>
+          <p className="popup-preview__description">{card.name}</p>
+        </div>
+      </div>
+  );
+
+}
+
+export default ImagePopup;
